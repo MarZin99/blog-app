772bd10e968cb276138410e1505bec4d090be049
Marcin Zawadzki <mar.zawadzki99@gmail.com>
2024-08-08 14:25:58 +0200

    Added top navigation bar, global css variables

---
+++ src/app/components/navigation/top-nav/top-nav.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'top-nav',
+  standalone: true,
+  imports: [],
+  templateUrl: './top-nav.component.html',
+  styleUrl: './top-nav.component.less',
+})
+export class TopNavComponent {}
